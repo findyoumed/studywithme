3f3188574d30d6003be4a3c374d99c4581232069
findyoumed <postnews@daum.net>
2026-01-31 09:18:02 +0900

    feat: update google analytics measurement id to G-NCTSTXSJ8D

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -1,5 +1,5 @@
-// Google Analytics 4
-const GA_ID = 'G-G6LB7SMP96';
+// [LOG: 20260131_0918] Update Google Analytics Measurement ID
+const GA_ID = 'G-NCTSTXSJ8D';
 
 // Inject gtag.js
 const script = document.createElement('script');
@@ -13,8 +13,8 @@ gtag('js', new Date());
 gtag('config', GA_ID);
 
 // Microsoft Clarity
-(function(c,l,a,r,i,t,y){
-    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+(function (c, l, a, r, i, t, y) {
+    c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+    t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
+    y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
 })(window, document, "clarity", "script", "v6wrgc61ts");
